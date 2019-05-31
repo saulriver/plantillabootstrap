da8fe937523a39f9fc198401d613a4f0bd356ad4
lauramuno <48606544+lauramuno@users.noreply.github.com>
2019-05-31 17:09:40 -0500

    se quita el tooltip en el menu

---
+++ docs/js/main.js
@@ -0,0 +1,117 @@
+(function() {
+    "use strict";
+
+    var treeviewMenu = $('.app-menu');
+
+    // Toggle Sidebar
+    $('[data-toggle="sidebar"]').click(function(event) {
+        event.preventDefault();
+        $('.app').toggleClass('sidenav-toggled');
+    });
+
+    // Activate sidebar treeview toggle
+    $("[data-toggle='treeview']").click(function(event) {
+        event.preventDefault();
+        if (!$(this).parent().hasClass('is-expanded')) {
+            treeviewMenu.find("[data-toggle='treeview']").parent().removeClass('is-expanded');
+        }
+        $(this).parent().toggleClass('is-expanded');
+    });
+
+    // Set initial active toggle
+    $("[data-toggle='treeview.'].is-expanded").parent().toggleClass('is-expanded');
+
+    //Activate bootstrip tooltips
+    $("[data-toggle='tooltip']").tooltip();
+
+})();
+
+//new js
+
+//cambio de letra y aumento
+$(document).ready(function() {
+
+    // Donde queremos cambiar el tamaño de la fuente
+    var donde = $('.bs-component');
+    var sizeFuenteOriginal = donde.css('font-size');
+
+    // Resetear Font Size
+    $(".resetearFont").click(function() {
+        donde.css('font-size', sizeFuenteOriginal);
+    });
+
+    // Aumentar Font Size
+    $(".aumentarFont").click(function() {
+
+        var sizeFuenteActual = donde.css('font-size');
+        var sizeFuenteActualNum = parseFloat(sizeFuenteActual, 10);
+        var sizeFuenteNuevo = sizeFuenteActualNum = 24;
+        donde.css('font-size', sizeFuenteNuevo);
+        return false;
+    });
+
+    // Disminuir Font Size
+    $(".disminuirFont").click(function() {
+        var sizeFuenteActual = donde.css('font-size');
+        var sizeFuenteActualNum = parseFloat(sizeFuenteActual, 10);
+        var sizeFuenteNuevo = sizeFuenteActualNum * 0.8;
+        donde.css('font-size', sizeFuenteNuevo);
+        return false;
+    });
+
+});
+
+//cambiar tipo fuente
+
+$(document).ready(function() {
+    $("#cambiar").click(function() {
+        $(".section").css({ 'font-family': 'Open Sans' });
+    });
+    $("#cambiar1").click(function() {
+        $(".section").css({ 'font-family': 'Montserrat' });
+    });
+    $("#cambiar2").click(function() {
+        $(".section").css({ 'font-family': 'Ubuntu Condensed' });
+    });
+});
+
+//cambio de color
+
+$(document).ready(function() {
+    $("#color").click(function() {
+        $(".container-fluid").css({ 'background-color': 'rgb(189, 202, 219)' });
+    });
+    $("#color1").click(function() {
+        $(".body").css({ 'font-family': 'Montserrat' });
+    });
+    $("#color2").click(function() {
+        $(".body").css({ 'font-family': 'Ubuntu Condensed' });
+    });
+});
+
+
+//para cambiar el tema color de la pagina
+
+function cambia(boton) {
+
+    var color = boton.name;
+    document.getElementById("prueba").style.background = color;
+
+    var color = boton.name;
+    document.getElementById("prueba2").style.background = color;
+
+    var color = boton.name;
+    document.getElementById("prueba3").style.background = color;
+}
+
+//paginacion para las vistas
+
+function handlePagination(e) {
+    if (isModifiedEvent(e) || !isLeftClickEvent(e)) {
+        return
+    }
+    e.stopPropagation();
+    e.preventDefault();
+    var url = $(this).attr("href ");
+    if (url) handleNavigation(url, true)
+}
