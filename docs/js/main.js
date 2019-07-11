5e4611a0deb97fcd8a0681667361836bc06c7319
saulriver <42225276+saulriver@users.noreply.github.com>
2019-07-10 19:00:59 -0500

    actual

--- docs/js/main.js
+++ docs/js/main.js
@@ -146,34 +146,3 @@ function cargar_pagina() { alert("Bienvenidos!"); }
     }, 50);
 });
 //buscador de palabras en el html
-var TRange = null
-
-function busca(str) {
-    if (parseInt(navigator.appVersion) < 4) return;
-    var strFound;
-    if (window.find) {
-
-        strFound = self.find(str);
-        if (!strFound) {
-            strFound = self.find(str, 0, 1)
-            while (self.find(str, 0, 1)) continue
-        }
-    } else if (navigator.appName.indexOf("Microsoft,Google") != -1) {
-
-        if (TRange != null) {
-            TRange.collapse(false)
-            strFound = TRange.findText(str)
-            if (strFound) TRange.select()
-        }
-        if (TRange == null || strFound == 0) {
-            TRange = self.document.body.createTextRange()
-            strFound = TRange.findText(str)
-            if (strFound) TRange.select()
-        }
-    } else if (navigator.appName == "Opera") {
-        alert("Opera no soporta busqueda")
-        return;
-    }
-    if (!strFound) alert("Palabra '" + str + "' no fue Encontrada")
-    return;
-}
